cea1abbca1c54291778635e9658c79a15ed7059a
Kathy Tran <ktran2@ufl.edu>
2023-03-22 18:49:19 -0400

    adding the decoded program to Meghana's code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Meghana's code
-# partner - Kathy
 def encode(number):
     encoded_password = ""
     if len(number) == 8: # this if statement ensures that this function works only is input is 8 characters long
@@ -10,22 +9,28 @@ def encode(number):
 
 def menu(): # menu function prints the display menu
     print("Menu")
-    print("-----------")
+    print("-------------")
     print("1.Encode")
     print("2.Decode")
     print("3.Quit")
 
 def main():
     run = True # run set to Boolean True for the while loop
+    number = 0
+    oldNumber = ''locals()
+
     while run:
+
         menu() # displays menu
         menu_option = int(input("Please enter an option: ")) # int() converts input to integer
         if menu_option == 1: # if menu choice entered is 1, user's input is encoded
-            user_input = input("Please enter your password to encode: ")
-            encode(user_input)
+            number = str(input("Please enter your password to encode: "))
+            oldNumber = number
+            encode(number)
             print("Your password has been encoded and stored!")
-        elif menu_option == 2:
-            pass # pass used to ensure the code runs without the decode function
+
+        elif menu_option == 2: # when option 2 is selected, the encoded password and the decoded str will be displayed
+            print(f"The encoded password is {encode(number)}, and the original password is {oldNumber}.")
         elif menu_option == 3:
             run = False   # run is set to Boolean false so that option 3 exists the while loop
 
